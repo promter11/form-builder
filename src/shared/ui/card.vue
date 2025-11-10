83d0c6c822abd598f06533166b1f7f336494c993
Aleksandr Samylin <promter11@gmail.com>
2025-11-11 01:43:13 +0300

    feat: add card component and configure import eslint rules

---
+++ src/shared/ui/card.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="$style.root">
+    <slot />
+  </div>
+</template>
+
+<style module lang="postcss">
+.root {
+  padding: 12px 16px;
+  border: 1px solid var(--color-border);
+  border-radius: 12px;
+  background-color: var(--color-surface);
+}
+</style>
